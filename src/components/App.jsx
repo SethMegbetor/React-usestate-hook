b371cc567cc3c7f2b114ce501c836c8e1bab7e0f
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-15 18:53:35 +0000

    recreated the app

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function increase() {
+    setCount(count + 1)
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
