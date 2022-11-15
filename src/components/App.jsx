b21e5ab00ccedfbbc320b4cd0786e0e0a2342754
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-15 17:55:19 +0000

    created a decrease function for the count

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState(23344);
+  // const state =
 
-  console.log(state[0]);
+  const [count, setCount] = useState(0);
+
   function increase() {
-    // count++;
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
   }
 
   return (
     <div className="container">
-      <h1>{state[0]}</h1>
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
   );
